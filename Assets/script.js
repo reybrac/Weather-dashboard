3f8a63e7f2ddcd7583bada731d797adfe6061047
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-02-04 14:40:20 -0800

    Added cards for the 5 day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,10 @@
 //api key - 007b58b57ff2308d83f8a775c2291ca5
 //api key - c6ac65a79b2de0a72c3f209350782695
 
-var formEl = $('#skills-form');
-var nameInputEl = $('#skill-name');
-// var dateInputEl = $('#datepicker');
-var skillsListEl = $('#skills-list');
+var formEl = $('#city-form');
+var nameInputEl = $('#city-name');
+
+var skillsListEl = $('#city-list');
 
 var printSkills = function (name) {
   var listEl = $('<li>');
@@ -13,11 +13,13 @@ var printSkills = function (name) {
   listEl.appendTo(skillsListEl);
 };
 
+
+
 var handleFormSubmit = function (event) {
   event.preventDefault();
 
   var nameInput = nameInputEl.val();
-//   var dateInput = dateInputEl.val();
+
 
   if (!nameInput ) {
     console.log('You need to fill out the form!');
@@ -28,58 +30,43 @@ var handleFormSubmit = function (event) {
 
   // resets form
   nameInputEl.val('');
-//   dateInputEl.val('');
+  $(".card-title").text(nameInput);
+
+  cityName = nameInput;
+  callApi1();
+
 };
+
+var cityName = "";
+
 
 formEl.on('submit', handleFormSubmit);
 
-// Autocomplete widget
-// $(function () {
-//   var skillNames = [
-//     'Bootstrap',
-//     'C',
-//     'C++',
-//     'CSS',
-//     'Express.js',
-//     'Git',
-//     'HTML',
-//     'Java',
-//     'JavaScript',
-//     'jQuery',
-//     'JSON',
-//     'MySQL',
-//     'Node.js',
-//     'NoSQL',
-//     'PHP',
-//     'Python',
-//     'React',
-//     'Ruby',
-//   ];
-//   $('#skill-name').autocomplete({
-//     source: skillNames,
-//   });
-// });
 
-// Datepicker widget
-// $(function () {
-//   $('#datepicker').datepicker({
-//     changeMonth: true,
-//     changeYear: true,
-//   });
-// });
-var cityName = "Tracy";
 
-var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=007b58b57ff2308d83f8a775c2291ca5";
-console.log("requestUrl: ", requestUrl);
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('weathermap Data \n----------');
-    console.log("data: ", data);
-    for (var i = 0; i < data.length; i++) {
-      console.log(data[i].name);
-      console.log(data[i].id);
-    }
-  });
+function callApi1(){
+    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=007b58b57ff2308d83f8a775c2291ca5";
+    console.log("requestUrl: ", requestUrl);
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log('weathermap Data \n----------');
+        console.log("data: ", data);
+        for (var i = 0; i < data.length; i++) {
+        console.log("name: ", data[i].name);
+        console.log("temp: ", data.main.temp);
+        }
+        console.log("temp: ", data.main.temp);
+        console.log("humidity: ", data.main.humidity);
+        console.log("speed: ", data.wind.speed);
+        if(cityName === ""){
+            return;
+        }else{
+            $("#temperature").text("Temperature: " + data.main.temp + " F");
+            $("#humidity").text("Humidity: " + data.main.humidity + " %");
+            $("#wind-speed").text("Wind speed: " + data.wind.speed + " MPH");
+        }
+    });
+}
