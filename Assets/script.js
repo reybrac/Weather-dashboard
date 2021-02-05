f46e5e51aa2f0be0aa56621c597cb281f136a1ad
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-02-04 16:58:55 -0800

    Adding second API call to add UV data

--- Assets/script.js
+++ Assets/script.js
@@ -54,7 +54,7 @@ var cityName = " ";
 formEl.on('submit', handleFormSubmit);
 
 
-
+//API call to get the information by the city name
 function callApi1(){
     var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=007b58b57ff2308d83f8a775c2291ca5";
     console.log("requestUrl: ", requestUrl);
@@ -78,6 +78,34 @@ function callApi1(){
             $("#temperature").text("Temperature: " + data.main.temp + " F");
             $("#humidity").text("Humidity: " + data.main.humidity + " %");
             $("#wind-speed").text("Wind speed: " + data.wind.speed + " MPH");
+        }
+    });
+}
+
+// API to call the UV data
+function callApi2(){
+    var requestUrl2 = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=007b58b57ff2308d83f8a775c2291ca5";
+    console.log("requestUrl2: ", requestUrl2);
+    fetch(requestUrl2)
+    .then(function (response2) {
+        return response2.json();
+    })
+    .then(function (data2) {
+        console.log('weathermap Data \n----------');
+        console.log("data2: ", data2);
+        for (var i = 0; i < data2.length; i++) {
+        console.log("name: ", data2[i].name);
+        console.log("temp: ", data2.main.temp);
+        }
+        console.log("temp: ", data2.main.temp);
+        console.log("humidity: ", data2.main.humidity);
+        console.log("speed: ", data2.wind.speed);
+        if(cityName === ""){
+            return;
+        }else{
+            $("#temperature").text("Temperature: " + data2.main.temp + " F");
+            $("#humidity").text("Humidity: " + data2.main.humidity + " %");
+            $("#wind-speed").text("Wind speed: " + data2.wind.speed + " MPH");
         }
     });
 }
