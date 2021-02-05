d6fa416ee0d8bba6ffbb57ab8ce5bafaa490193c
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-02-04 22:13:00 -0800

    Added second API call for the UV index

--- Assets/script.js
+++ Assets/script.js
@@ -79,33 +79,34 @@ function callApi1(){
             $("#humidity").text("Humidity: " + data.main.humidity + " %");
             $("#wind-speed").text("Wind speed: " + data.wind.speed + " MPH");
         }
+        callApi2(data.coord.lat, data.coord.lon);
     });
 }
 
 // API to call the UV data
-function callApi2(){
-    var requestUrl2 = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=007b58b57ff2308d83f8a775c2291ca5";
+function callApi2(lat, longe){
+    var requestUrl2 = "http://api.openweathermap.org/data/2.5/uvi?appid=007b58b57ff2308d83f8a775c2291ca5&lat=" + lat + "&lon=" + longe;
     console.log("requestUrl2: ", requestUrl2);
     fetch(requestUrl2)
     .then(function (response2) {
         return response2.json();
     })
     .then(function (data2) {
-        console.log('weathermap Data \n----------');
-        console.log("data2: ", data2);
-        for (var i = 0; i < data2.length; i++) {
-        console.log("name: ", data2[i].name);
-        console.log("temp: ", data2.main.temp);
-        }
-        console.log("temp: ", data2.main.temp);
-        console.log("humidity: ", data2.main.humidity);
-        console.log("speed: ", data2.wind.speed);
-        if(cityName === ""){
-            return;
-        }else{
-            $("#temperature").text("Temperature: " + data2.main.temp + " F");
-            $("#humidity").text("Humidity: " + data2.main.humidity + " %");
-            $("#wind-speed").text("Wind speed: " + data2.wind.speed + " MPH");
-        }
+        console.log('Data2 ', data2);
+        // console.log("data2: ", data2);
+        // for (var i = 0; i < data2.length; i++) {
+        // console.log("name: ", data2[i].name);
+        // console.log("temp: ", data2.main.temp);
+        // }
+        // console.log("temp: ", data2.main.temp);
+        // console.log("humidity: ", data2.main.humidity);
+        // console.log("speed: ", data2.wind.speed);
+        // if(cityName === ""){
+        //     return;
+        // }else{
+        //     $("#temperature").text("Temperature: " + data2.main.temp + " F");
+        //     $("#humidity").text("Humidity: " + data2.main.humidity + " %");
+        //     $("#wind-speed").text("Wind speed: " + data2.wind.speed + " MPH");
+        // }
     });
 }
